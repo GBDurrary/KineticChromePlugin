12905e14741943e5243a931c39486240597e1c22
GBDurrary <ryan.durrant@progressive-technology.co.uk>
2024-08-29 10:44:09 +0100

    Show styles per environment and globalise most styling changes

--- content.js
+++ content.js
@@ -1,15 +1,21 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // var currentUrl = window.location.href.toLowerCase();
-    // var stylesFile = 'styles.css'; 
-    // if (currentUrl.includes("kinetictest")) {
-    //     stylesFile = 'testStyles.css';
-    // } else if (currentUrl.includes("kineticdev")) {
-    //     stylesFile = 'devStyles.css';
-    // }
+const loadStyles = function() {
+    var currentUrl = window.location.href.toLowerCase();
+    var stylesFile = 'liveStyles.css'; 
+    if (currentUrl.includes("kinetictest")) {
+        stylesFile = 'testStyles.css';
+    } else if (currentUrl.includes("kineticdev")) {
+        stylesFile = 'devStyles.css';
+    }
 
     const link = document.createElement('link');
     link.rel = 'stylesheet';
     link.type = 'text/css';
-    link.href = chrome.runtime.getURL('styles.css');
+    link.href = chrome.runtime.getURL(stylesFile);
     document.head.appendChild(link);
-});
+};
+
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', loadStyles);
+} else {
+    loadStyles();
+}
