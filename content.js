b87aea3531615cfd97b0d128f3b094b9759c9594
GBDurrary <ryan.durrant@progressive-technology.co.uk>
2024-08-29 09:06:21 +0100

    Match urls in manifest - different colour for each environment

--- content.js
+++ content.js
@@ -1,4 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
+    // var currentUrl = window.location.href.toLowerCase();
+    // var stylesFile = 'styles.css'; 
+    // if (currentUrl.includes("kinetictest")) {
+    //     stylesFile = 'testStyles.css';
+    // } else if (currentUrl.includes("kineticdev")) {
+    //     stylesFile = 'devStyles.css';
+    // }
+
     const link = document.createElement('link');
     link.rel = 'stylesheet';
     link.type = 'text/css';
